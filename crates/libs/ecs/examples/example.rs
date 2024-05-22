d683cf66b131f5dcdbb03ae4f4874faf07091df6
JohnLee <lijiang01001@163.com>
2024-05-22 22:59:26 +0800

    bevy_ecs test

--- crates/libs/ecs/examples/example.rs
+++ crates/libs/ecs/examples/example.rs
@@ -1,129 +1,55 @@
-use syn::{parse::{Parse, ParseStream}, token::Comma, Ident, LitInt};
+use bevy_ecs::{bundle, entity, prelude::*};
 
-// 描述宏需要解析到的数据
-struct AllTuples {
-    macro_ident: Ident,
-    start: usize,
-    end: usize,
-    ident: Ident,
+#[derive(Component)]
+struct Position {
+    x: f32,
+    y: f32,
+}
+#[derive(Component)]
+struct Player;
+#[derive(Component)]
+struct Alive;
+#[derive(Component)]
+struct Velocity {
+    x: f32,
+    y: f32,
 }
 
-impl Parse for AllTuples {
-    fn parse(input: ParseStream) -> Result<Self> {
-        let macro_ident = input.parse::<Ident>()?; // 解析使用哪个宏定义
-        input.parse::<Comma>()?;// 解析逗号
-        let start = input.parse::<LitInt>()?.base10_parse()?; // 解析开始数字
-        input.parse::<Comma>()?;// 解析逗号
-        let end = input.parse::<LitInt>()?.base10_parse()?;// 解析结束数字
-        input.parse::<Comma>()?;// 解析逗号
-        let ident =input.parse::<Ident>()?; // 解析使用什么去定义泛型参数
-
-        Ok(AllTuples {
-            macro_ident,
-            start,
-            end,
-            ident,
-        })
+fn movement(mut query: Query<(&mut Position, &Velocity)>) {
+    for (mut position, velocity) in &mut query {
+        position.x += velocity.x;
+        position.y += velocity.y;
     }
 }
 
-pub fn all_tuples(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as AllTuples);
-    let len = input.end - input.start;
-    let mut ident_tuples = Vec::with_capacity(len);
-    for i in input.start..=input.end {
-        let ident = format_ident!("{}{}", input.ident, i);
-        ident_tuples.push(quote! {
-            #ident
-        });
-    }
-
-    let macro_ident = &input.macro_ident;
-    let invocations = (input.start..=input.end).map(|i| {
-        let ident_tuples = &ident_tuples[..i];
-        quote! {
-            #macro_ident!(#(#ident_tuples),*);
-        }
-    });
-    TokenStream::from(quote! {
-        #(
-            #invocations
-        )*
-    })
-}
-
-macro_rules! impl_tuple_system_param {
-    ($($param: ident),*) => {
-        impl<$($param: SystemParam,)*> SystemParam
-            for ($($param,)*)
-        {
-        }
-    };
-}
-
-all_tuples!(impl_system_param_functions, 0, 15, P);
-
-pub trait SystemParamFunction<Param>: 'static {
-    fn run(&mut self, params: Param);
-}
-
-impl<F, Param> System for F
-where
-    F: SystemParamFunction<Param>,
-{
-    fn run(&mut self) {
-        self.run(todo!())
+fn print(mut query: Query<(&mut Position, &Velocity)>) {
+    for (mut position, velocity) in &mut query {
+        println!("{} {} {}", position.x, position.y, velocity.x);
     }
 }
 
-pub trait SystemParam {}
-
-impl SystemParam for u32 {}
-impl SystemParam for String {}
-impl SystemParam for bool {}
-
-pub trait System {
-    fn run(&mut self);
-}
-
-pub struct App {
-    systems: Vec<Box<dyn System>>,
-}
-
-impl App {
-    pub fn new() -> Self {
-        Self { systems: vec![] }
-    }
-
-    pub fn add_system<T: System + 'static>(&mut self, system: T) -> &mut Self {
-        self.systems.push(Box::new(system));
-        self
-    }
-
-    pub fn run(&mut self) {
-        for item in self.systems.iter_mut() {
-            item.run();
-        }
+fn system_changed(query: Query<&Position, Changed<Velocity>>) {
+    for position in &query {
     }
 }
 
-impl<T: FnMut()> System for T {
-    fn run(&mut self) {
-        self();
+fn system_added(query: Query<&Position, Added<Velocity>>) {
+    for position in &query {
     }
 }
 
 fn main() {
-    App::new()
-        .add_system(test_system)
-        .add_system(test_system2)
-        .run();
-}
+    let mut world = World::new();
 
-fn test_system() {
-    println!("test_system");
-}
+    let bundle = (Position { x: 0.0, y: 0.0 }, Velocity { x: 1.0, y: 0.0 });
+    let mut entity: EntityWorldMut = world.spawn(bundle);
+    entity.insert(Alive);
 
-fn test_system2() {
-    println!("test_system2");
+    let mut schedule = Schedule::default();
+
+    schedule.add_systems(movement).add_systems(print);
+
+    loop {
+        schedule.run(&mut world);
+    }
 }
