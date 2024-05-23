8bb7b69d9da594d01cf03ef0ffca27328a596011
JohnLee <lijiang01001@163.com>
2024-05-23 23:01:50 +0800

    测试 bundle remove add....

--- crates/libs/ecs/examples/example.rs
+++ crates/libs/ecs/examples/example.rs
@@ -6,8 +6,6 @@ struct Position {
     y: f32,
 }
 #[derive(Component)]
-struct Player;
-#[derive(Component)]
 struct Alive;
 #[derive(Component)]
 struct Velocity {
@@ -15,10 +13,38 @@ struct Velocity {
     y: f32,
 }
 
-fn movement(mut query: Query<(&mut Position, &Velocity)>) {
-    for (mut position, velocity) in &mut query {
-        position.x += velocity.x;
-        position.y += velocity.y;
+#[derive(Resource)]
+struct GlobalPosMinMax {
+    min: f32,
+    max: f32,
+}
+
+impl FromWorld for GlobalPosMinMax {
+    fn from_world(world: &mut World) -> Self {
+        GlobalPosMinMax {
+            min: 0.0,
+            max: 10000.0,
+        }
+    }
+}
+
+fn movement(
+    mut query: Query<(&Alive, &mut Position, &Velocity)>,
+    mut goals: ResMut<GlobalPosMinMax>,
+) {
+    for (_, mut position, velocity) in &mut query {
+        if ((position.x + velocity.x) >= goals.min && (position.x + velocity.x) <= goals.max) {
+            position.x += velocity.x;
+            position.y += velocity.y;
+        }
+    }
+}
+
+fn death(mut query: Query<(Entity, &Alive, &mut Position)>, mut commands: Commands) {
+    for (entity, _, mut position) in &mut query {
+        if (position.x >= 8000.0) {
+            commands.entity(entity).remove::<Alive>();
+        }
     }
 }
 
@@ -28,26 +54,27 @@ fn print(mut query: Query<(&mut Position, &Velocity)>) {
     }
 }
 
-fn system_changed(query: Query<&Position, Changed<Velocity>>) {
-    for position in &query {
-    }
-}
-
-fn system_added(query: Query<&Position, Added<Velocity>>) {
-    for position in &query {
-    }
-}
-
 fn main() {
     let mut world = World::new();
 
-    let bundle = (Position { x: 0.0, y: 0.0 }, Velocity { x: 1.0, y: 0.0 });
-    let mut entity: EntityWorldMut = world.spawn(bundle);
-    entity.insert(Alive);
+    let bundle1 = (
+        Alive,
+        Position { x: 0.0, y: 0.0 },
+        Velocity { x: 1.0, y: 0.0 },
+    );
+    let bundle2 = (
+        Alive,
+        Position { x: 0.0, y: 0.0 },
+        Velocity { x: 2.0, y: 0.0 },
+    );
+    world.spawn(bundle1);
+    world.spawn(bundle2);
+
+    world.init_resource::<GlobalPosMinMax>();
 
     let mut schedule = Schedule::default();
 
-    schedule.add_systems(movement).add_systems(print);
+    schedule.add_systems(movement).add_systems(print).add_systems(death);
 
     loop {
         schedule.run(&mut world);
