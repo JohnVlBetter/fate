c0208e8c14d6f4de9d12d261a3d1c99067708ea5
JohnLee <lijiang01001@163.com>
2024-05-29 23:07:38 +0800

    音频播放器

--- crates/libs/audio/examples/audio_example.rs
+++ crates/libs/audio/examples/audio_example.rs
@@ -1,34 +1,19 @@
-use std::fs::File;
-use std::io::BufReader;
-use std::path::Path;
-use std::time::Duration;
-
 use audio::audio_player::AudioPlayer;
 use audio::audio_source::{AudioLoader, AudioSource};
 use resource::resource_mgr::ResourceMgr;
-use rodio::queue::SourcesQueueOutput;
-use rodio::{Decoder, OutputStream, Sink, Source};
+use std::path::Path;
 
 fn main() {
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-
-    let sink = Sink::try_new(&stream_handle).unwrap();
-
-    let sound_duration = Duration::from_millis(1000);
-
-    let source = rodio::source::SineWave::new(1000.0).take_duration(sound_duration);
-    sink.append(source);
-
-    sink.sleep_until_end();
-
-    /*  std::thread::sleep(std::time::Duration::from_secs(5));
     let mut mgr = ResourceMgr::new();
     mgr.register_loader(AudioLoader::default());
-    let loader = mgr.get_asset_loader_with_extension("ogg");
-    println!("{}", loader.unwrap().extensions()[0]);
-    let resource = mgr.load(&Path::new("assets/audio/breakout_collision.ogg"));
+    let resource = mgr.load(&Path::new("assets/audio/Windless Slopes.ogg"));
     let binding = resource.unwrap();
     let resource = binding.as_any().downcast_ref::<AudioSource>().unwrap();
-    println!("{}", resource.bytes.len());
-    AudioPlayer::default().play(resource.clone());*/
+    let player = AudioPlayer::default();
+    player.play(resource);
+    std::thread::sleep(std::time::Duration::from_secs(1));
+    player.pause();
+    std::thread::sleep(std::time::Duration::from_secs(10));
+    player.resume(resource);
+    std::thread::sleep(std::time::Duration::from_secs(10000));
 }
--- crates/libs/audio/src/audio_player.rs
+++ crates/libs/audio/src/audio_player.rs
@@ -4,41 +4,81 @@ use crate::audio_source::{self, Decodable};
 
 pub struct AudioPlayer {
     stream_handle: Option<OutputStreamHandle>,
+    left_ear: [f32; 3],
+    right_ear: [f32; 3],
+    emitter_translation: [f32; 3],
 }
 
 impl Default for AudioPlayer {
     fn default() -> Self {
+        let emitter_translation = [0.0, 0.0, 0.0];
+        let (left_ear, right_ear) = ([-2.0, 0.0, 0.0], [2.0, 0.0, 0.0]);
         if let Ok((stream, stream_handle)) = OutputStream::try_default() {
             std::mem::forget(stream);
             Self {
                 stream_handle: Some(stream_handle),
+                left_ear,
+                right_ear,
+                emitter_translation,
             }
         } else {
             println!("找不到输出设备!");
             Self {
                 stream_handle: None,
+                left_ear,
+                right_ear,
+                emitter_translation,
             }
         }
     }
 }
 
+//TODO: 这里暂停和恢复有问题，目前看来还是需要单开线程去做
 impl AudioPlayer {
-    pub fn play(&self, audio_source: audio_source::AudioSource) {
-        let Some(stream_handle) = self.stream_handle.as_ref() else {
-            return;
-        };
-        let emitter_translation = [0.0, 0.0, 0.0];
-        let (left_ear, right_ear) = ([-2.0, 0.0, 0.0], [2.0, 0.0, 0.0]);
-        let sink =
-            match SpatialSink::try_new(stream_handle, emitter_translation, left_ear, right_ear) {
-                Ok(sink) => sink,
-                Err(err) => {
-                    println!("Error creating spatial sink: {err:?}");
-                    return;
-                }
-            };
-        sink.set_speed(1.0);
-        sink.set_volume(1.0);
+    pub fn play(&self, audio_source: &audio_source::AudioSource) {
+        let sink = self.create().unwrap();
         sink.append(audio_source.decoder().repeat_infinite());
+        sink.detach();
+    }
+
+    pub fn pause(&self) {
+        let sink = self.create().unwrap();
+        sink.pause();
+        sink.detach();
+    }
+
+    pub fn resume(&self, audio_source: &audio_source::AudioSource) {
+        let sink = self.create().unwrap();
+        if sink.is_paused() {
+            println!("111");
+            sink.play();
+        }else{
+            sink.append(audio_source.decoder().repeat_infinite());
+        }
+        sink.detach();
+    }
+
+    pub fn set_speed(&self, speed: f32) {
+        let sink = self.create().unwrap();
+        sink.set_speed(speed);
+        sink.detach();
+    }
+
+    fn create(&self) -> Option<SpatialSink> {
+        let Some(stream_handle) = self.stream_handle.as_ref() else {
+            return None;
+        };
+        match SpatialSink::try_new(
+            stream_handle,
+            self.emitter_translation,
+            self.left_ear,
+            self.right_ear,
+        ) {
+            Ok(sink) => Some(sink),
+            Err(err) => {
+                println!("创建音轨失败: {err:?}");
+                None
+            }
+        }
     }
 }
