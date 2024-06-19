9327224c12ce75233dbb8b19017b56e15ccb5fe8
lijiang <lijiang01001@163.com>
2024-06-19 13:04:04 +0800

    glam transform

--- crates/libs/scene/src/transform.rs
+++ crates/libs/scene/src/transform.rs
@@ -1,4 +1,6 @@
-use cgmath::{Deg, Euler, Matrix4, SquareMatrix, Vector3, Zero};
+use std::ops::Mul;
+
+use glam::{Affine3A, Mat3, Mat4, Quat, Vec3};
 
 use crate::component::{Component, ComponentBase};
 
@@ -6,11 +8,10 @@ use crate::component::{Component, ComponentBase};
 pub struct Transform {
     pub(crate) id: u32,
     pub(crate) node_id: u32,
-    pub(crate) position: Vector3<f32>,
-    pub(crate) rotation: Vector3<f32>,
-    pub(crate) scale: Vector3<f32>,
-    pub(crate) local_to_world_matrix: Matrix4<f32>,
-    pub(crate) world_to_local_matrix: Matrix4<f32>,
+    pub translation: Vec3,
+    pub rotation: Quat,
+    pub scale: Vec3,
+    pub(crate) local_to_world_matrix: Affine3A,
     pub(crate) dirty: bool,
 }
 
@@ -27,79 +28,280 @@ impl PartialEq for Transform {
 }
 
 impl Transform {
-    pub fn new(position: Vector3<f32>, rotation: Vector3<f32>, scale: Vector3<f32>) -> Self {
-        let translation_mat = Matrix4::from_translation(Vector3::from(position));
-        let rotation_mat = Matrix4::from(Euler::new(
-            Deg(rotation.x),
-            Deg(rotation.y),
-            Deg(rotation.z),
-        ));
-        let scale_mat = Matrix4::from_nonuniform_scale(scale.x, scale.y, scale.z);
-        let local_to_world_matrix = translation_mat * rotation_mat * scale_mat;
-        let world_to_local_matrix = local_to_world_matrix
-            .invert()
-            .unwrap_or(Matrix4::identity());
-        Self {
+    #[inline]
+    pub fn from_xyz(x: f32, y: f32, z: f32) -> Self {
+        Self::from_translation(Vec3::new(x, y, z))
+    }
+
+    #[inline]
+    pub fn from_translation(translation: Vec3) -> Self {
+        Transform {
             id: 0,
             node_id: 0,
-            position,
-            rotation,
-            scale,
-            local_to_world_matrix,
-            world_to_local_matrix,
+            translation,
+            rotation: Quat::IDENTITY,
+            scale: Vec3::ONE,
+            local_to_world_matrix: Affine3A::from_translation(translation),
             dirty: false,
         }
     }
 
-    pub fn position(&self) -> Vector3<f32> {
-        self.position
-    }
-
-    pub fn rotation(&self) -> Vector3<f32> {
-        self.rotation
-    }
-
-    pub fn scale(&self) -> Vector3<f32> {
-        self.scale
-    }
-
-    pub fn translate(&mut self, position: Vector3<f32>) {
-        self.position += position;
-        self.dirty = true;
-    }
-
-    pub fn rotate(&mut self, rotation: Vector3<f32>) {
-        self.rotation += rotation;
-        self.dirty = true;
-    }
-
-    pub fn set_position(&mut self, position: Vector3<f32>) {
-        self.position = position;
-        self.dirty = true;
-    }
-
-    pub fn set_rotation(&mut self, rotation: Vector3<f32>) {
-        self.rotation = rotation;
-        self.dirty = true;
-    }
-
-    pub fn set_scale(&mut self, scale: Vector3<f32>) {
-        self.scale = scale;
-        self.dirty = true;
-    }
-
-    pub fn local_to_world_matrix(&mut self) -> Matrix4<f32> {
-        if self.dirty {
-            self.update();
+    #[inline]
+    pub fn from_rotation(rotation: Quat) -> Self {
+        Transform {
+            id: 0,
+            node_id: 0,
+            translation: Vec3::ZERO,
+            rotation: rotation,
+            scale: Vec3::ONE,
+            local_to_world_matrix: Affine3A::from_rotation_translation(rotation, Vec3::ZERO),
+            dirty: false,
         }
+    }
+
+    #[inline]
+    pub fn from_scale(scale: Vec3) -> Self {
+        Transform {
+            id: 0,
+            node_id: 0,
+            translation: Vec3::ZERO,
+            rotation: Quat::IDENTITY,
+            scale: scale,
+            local_to_world_matrix: Affine3A::from_scale(scale),
+            dirty: false,
+        }
+    }
+
+    #[inline]
+    pub fn local_to_world_matrix(&self) -> Mat4 {
+        Mat4::from(self.local_to_world_matrix)
+    }
+
+    #[inline]
+    pub fn from_matrix(matrix: Mat4) -> Self {
+        let (scale, rotation, translation) = matrix.to_scale_rotation_translation();
+
+        Transform {
+            id: 0,
+            node_id: 0,
+            translation,
+            rotation,
+            scale,
+            local_to_world_matrix: Affine3A::from_scale_rotation_translation(
+                scale,
+                rotation,
+                translation,
+            ),
+            dirty: false,
+        }
+    }
+
+    #[inline]
+    #[must_use]
+    pub fn looking_at(mut self, target: Vec3, up: Vec3) -> Self {
+        self.look_at(target, up);
+        self
+    }
+
+    #[inline]
+    #[must_use]
+    pub fn looking_to(mut self, direction: Vec3, up: Vec3) -> Self {
+        self.look_to(direction, up);
+        self
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn with_translation(mut self, translation: Vec3) -> Self {
+        self.translation = translation;
+        self
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn with_rotation(mut self, rotation: Quat) -> Self {
+        self.rotation = rotation;
+        self
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn with_scale(mut self, scale: Vec3) -> Self {
+        self.scale = scale;
+        self
+    }
+
+    #[inline]
+    pub fn local_x(&self) -> Vec3 {
+        self.rotation * Vec3::X
+    }
+
+    #[inline]
+    pub fn left(&self) -> Vec3 {
+        -self.local_x()
+    }
+
+    #[inline]
+    pub fn right(&self) -> Vec3 {
+        self.local_x()
+    }
+
+    #[inline]
+    pub fn local_y(&self) -> Vec3 {
+        self.rotation * Vec3::Y
+    }
+
+    #[inline]
+    pub fn up(&self) -> Vec3 {
+        self.local_y()
+    }
+
+    #[inline]
+    pub fn down(&self) -> Vec3 {
+        -self.local_y()
+    }
+
+    #[inline]
+    pub fn local_z(&self) -> Vec3 {
+        self.rotation * Vec3::Z
+    }
+
+    #[inline]
+    pub fn forward(&self) -> Vec3 {
+        -self.local_z()
+    }
+
+    #[inline]
+    pub fn back(&self) -> Vec3 {
+        self.local_z()
+    }
+
+    #[inline]
+    pub fn rotate(&mut self, rotation: Quat) {
+        self.rotation = rotation * self.rotation;
+    }
+
+    #[inline]
+    pub fn rotate_axis(&mut self, axis: Vec3, angle: f32) {
+        self.rotate(Quat::from_axis_angle(axis, angle));
+    }
+
+    #[inline]
+    pub fn rotate_x(&mut self, angle: f32) {
+        self.rotate(Quat::from_rotation_x(angle));
+    }
+
+    #[inline]
+    pub fn rotate_y(&mut self, angle: f32) {
+        self.rotate(Quat::from_rotation_y(angle));
+    }
+
+    #[inline]
+    pub fn rotate_z(&mut self, angle: f32) {
+        self.rotate(Quat::from_rotation_z(angle));
+    }
+
+    #[inline]
+    pub fn rotate_local(&mut self, rotation: Quat) {
+        self.rotation *= rotation;
+    }
+
+    #[inline]
+    pub fn rotate_local_axis(&mut self, axis: Vec3, angle: f32) {
+        self.rotate_local(Quat::from_axis_angle(axis, angle));
+    }
+
+    #[inline]
+    pub fn rotate_local_x(&mut self, angle: f32) {
+        self.rotate_local(Quat::from_rotation_x(angle));
+    }
+
+    #[inline]
+    pub fn rotate_local_y(&mut self, angle: f32) {
+        self.rotate_local(Quat::from_rotation_y(angle));
+    }
+
+    #[inline]
+    pub fn rotate_local_z(&mut self, angle: f32) {
+        self.rotate_local(Quat::from_rotation_z(angle));
+    }
+
+    #[inline]
+    pub fn translate_around(&mut self, point: Vec3, rotation: Quat) {
+        self.translation = point + rotation * (self.translation - point);
+    }
+
+    #[inline]
+    pub fn rotate_around(&mut self, point: Vec3, rotation: Quat) {
+        self.translate_around(point, rotation);
+        self.rotate(rotation);
+    }
+
+    #[inline]
+    pub fn look_at(&mut self, target: Vec3, up: Vec3) {
+        self.look_to(target - self.translation, up);
+    }
+
+    #[inline]
+    pub fn look_to(&mut self, direction: Vec3, up: Vec3) {
+        let back = -direction.try_normalize().unwrap_or(Vec3::NEG_Z);
+        let up = up.try_normalize().unwrap_or(Vec3::Y);
+        let right = up
+            .cross(back)
+            .try_normalize()
+            .unwrap_or_else(|| up.any_orthonormal_vector());
+        let up = back.cross(right);
+        self.rotation = Quat::from_mat3(&Mat3::from_cols(right, up, back));
+    }
+
+    #[inline]
+    pub fn affine(&self) -> Affine3A {
         self.local_to_world_matrix
     }
 
-    pub fn world_to_local_matrix(&mut self) -> Matrix4<f32> {
-        if self.dirty {
-            self.update();
+    #[inline]
+    #[must_use]
+    pub fn mul_transform(&self, transform: Transform) -> Self {
+        let translation = self.transform_point(transform.translation);
+        let rotation = self.rotation * transform.rotation;
+        let scale = self.scale * transform.scale;
+        Transform {
+            id: 0,
+            node_id: 0,
+            translation,
+            rotation,
+            scale,
+            local_to_world_matrix: Affine3A::from_scale_rotation_translation(
+                scale,
+                rotation,
+                translation,
+            ),
+            dirty: false,
         }
-        self.world_to_local_matrix
+    }
+
+    #[inline]
+    pub fn transform_point(&self, mut point: Vec3) -> Vec3 {
+        point = self.scale * point;
+        point = self.rotation * point;
+        point += self.translation;
+        point
+    }
+}
+
+impl Mul<Transform> for Transform {
+    type Output = Transform;
+
+    fn mul(self, transform: Transform) -> Self::Output {
+        self.mul_transform(transform)
+    }
+}
+
+impl Mul<Vec3> for Transform {
+    type Output = Vec3;
+
+    fn mul(self, value: Vec3) -> Self::Output {
+        self.transform_point(value)
     }
 }
 
@@ -136,21 +338,7 @@ impl ComponentBase for Transform {
 }
 
 impl Transform {
-    fn update(&mut self) {
-        let translation_mat = Matrix4::from_translation(Vector3::from(self.position));
-        let rotation_mat = Matrix4::from(Euler::new(
-            Deg(self.rotation.x),
-            Deg(self.rotation.y),
-            Deg(self.rotation.z),
-        ));
-        let scale_mat = Matrix4::from_nonuniform_scale(self.scale.x, self.scale.y, self.scale.z);
-        self.local_to_world_matrix = translation_mat * rotation_mat * scale_mat;
-        self.world_to_local_matrix = self
-            .local_to_world_matrix
-            .invert()
-            .unwrap_or(Matrix4::identity());
-        self.dirty = false;
-    }
+    fn update(&mut self) {}
 }
 
 impl Default for Transform {
@@ -158,11 +346,10 @@ impl Default for Transform {
         Transform {
             id: 0,
             node_id: 0,
-            position: Vector3::zero(),
-            rotation: Vector3::zero(),
-            scale: Vector3::new(1.0, 1.0, 1.0),
-            local_to_world_matrix: Matrix4::identity(),
-            world_to_local_matrix: Matrix4::identity(),
+            translation: Vec3::ZERO,
+            rotation: Quat::IDENTITY,
+            scale: Vec3::ONE,
+            local_to_world_matrix: Affine3A::IDENTITY,
             dirty: false,
         }
     }
